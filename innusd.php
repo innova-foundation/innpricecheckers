f89d76b0719910d56dc81ccfee1272177833cf61
mangae <37798589+mangae@users.noreply.github.com>
2020-01-17 10:19:07 +0100

    Update and rename dnrusd.php to innusd.php

--- innusd.php
+++ innusd.php
@@ -1,14 +1,14 @@
 <?php
 
-$url = "https://api.coinmarketcap.com/v1/ticker/denarius-dnr/";
+$url = "https://api.coinmarketcap.com/v1/ticker/innova/";
 
 $json = file_get_contents($url);
 $dataparsed = json_decode($json, TRUE);
 
 $lastprice = $dataparsed[0]["price_usd"];
 
-$dnrusd = $lastprice;
+$innusd = $lastprice;
 
 ?>
 
-<?php print($dnrusd) ?>
+<?php print($innusd) ?>
