0e5737558c76feee4dce76d84e5a1b9c16a7081c
mangae <37798589+mangae@users.noreply.github.com>
2020-01-17 10:18:25 +0100

    Update and rename dnrmc.php to innmc.php

--- innmc.php
+++ innmc.php
@@ -1,14 +1,14 @@
 <?php
 
-$url = "https://api.coinmarketcap.com/v1/ticker/denarius-dnr/";
+$url = "https://api.coinmarketcap.com/v1/ticker/innova/";
 
 $json = file_get_contents($url);
 $dataparsed = json_decode($json, TRUE);
 
 $mc = $dataparsed[0]["market_cap_usd"];
 
-$dnrmc = $mc;
+$innmc = $mc;
 
 ?>
 
-<?php print($dnrmc) ?>
+<?php print($innmc) ?>
