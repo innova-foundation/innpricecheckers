f45a23a865fca2077335a26a2b470233440eff86
CircuitBreaker <44181392+CircuitBreaker88@users.noreply.github.com>
2020-04-05 08:18:00 -0400

    Update ibitcoin.php

--- ibitcoin.php
+++ ibitcoin.php
@@ -1,1 +1,14 @@
+<?php
 
+$url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd";
+
+$json = file_get_contents($url);
+$dataparsed = json_decode($json, TRUE);
+
+$lastprice = $dataparsed["bitcoin"]["usd"];
+
+$bitcoin = $lastprice;
+
+?>
+
+<?php print($bitcoin) ?>
